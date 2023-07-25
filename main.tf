b3d5afd9401a10675b4c07827b8f8e18f07b440c
Abdul Khadar Kutagolla <AL5422@accionlabs.in>
2023-07-25 18:59:58 +0530

    Add tf files

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.66.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name                = "RG-TerraForm-SA"
+    storage_account_name               = "saterraform7"
+    container_name                     = "tfstate"
+    key                                = "terraform.tfstate"
+}
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+  }
+}
+
+module "RG" {
+    source = "./ResourceGroups"
+}
