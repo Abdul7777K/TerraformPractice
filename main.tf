bb1730913f9ede15c1dc9b61a6188b08d903ec04
Abdul <Abdul@example.com>
2023-07-25 23:04:51 +0530

    removed provider vars

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name                = var.resource_group_name
-    storage_account_name               = var.storage_account_name
-    container_name                     = var.container_name
-    key                                = var.key
+    resource_group_name                = "RG-TerraForm-SA"
+    storage_account_name               = "saterraform7"
+    container_name                     = "tfstate"
+    key                                = "terraform.tfstate"
     use_oidc                           = true
 }
 }
