f0e4635ffa9b33cbcc3c4d4c51b4efc0cdf92836
Abdul <Abdul@example.com>
2023-07-26 13:05:32 +0530

    qwer

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 
   backend "azurerm" {
+    use_oidc = true
+    resource_group_name                = "RG-TerraForm-SA"
+    storage_account_name               = "saterraform7"
+    container_name                     = "tfstate"
+    key                                = "terraform.tfstate"
 }
 }
 
